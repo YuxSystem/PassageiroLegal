700c4a18aff22805de97ea7c50a9e7e787c1d02c
MTzedeki <multimedia2multicompanies@gmail.com>
2024-12-05 16:29:56 -0300

    Projeto VooLegal

---
+++ V1/resources/js/Pages/Web/Cookies.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="min-h-screen bg-gray-50">
+      <header class="bg-blue-600 text-white py-4">
+        <div class="container mx-auto">
+          <h1 class="text-2xl font-bold">Direito dos Passageiros Aéreos</h1>
+        </div>
+      </header>
+      <main class="container mx-auto my-8 px-4">
+        <p class="text-gray-700">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio.
+        </p>
+      </main>
+    </div>
+  </template>

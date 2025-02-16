47a8982dd4c3822662d8097853a359364288798b
Julio Santana <julio.santana@justa.com.vc>
2025-02-16 17:11:03 -0300

    fix(server): adiciona import faltante da interface SolicitationRepository

--- app/Repositories/Implementations/SolicitationRepositoryImpl.php
+++ app/Repositories/Implementations/SolicitationRepositoryImpl.php
@@ -3,9 +3,10 @@
 namespace App\Repositories\Implementations;
 
 use App\Models\Solicitations;
+use App\Repositories\SolicitationRepository;
 use Illuminate\Database\Eloquent\Collection;
 
-class SolicitationRepositoryImpl implements \SolicitationRepository
+class SolicitationRepositoryImpl implements SolicitationRepository
 {
     public function getAll(): Collection
     {

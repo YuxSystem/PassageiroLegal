c2860fbb6ca10453f30adaa7706a672a7689c90d
Julio Santana <julio.santana@justa.com.vc>
2025-02-16 22:27:52 -0300

    refactor(server): remove validacao do Service

--- app/Services/Implementations/SolicitationServiceImpl.php
+++ app/Services/Implementations/SolicitationServiceImpl.php
@@ -4,8 +4,6 @@ namespace App\Services\Implementations;
 
 use App\Repositories\SolicitationRepository;
 use App\Services\SolicitationService;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\ValidationException;
 
 class SolicitationServiceImpl implements SolicitationService
 {
@@ -21,20 +19,8 @@ class SolicitationServiceImpl implements SolicitationService
         return $this->solicitationRepository->getAll()->toArray();
     }
 
-    /**
-     * @throws ValidationException
-     */
     public function createSolicitation(array $data): array
     {
-        $validated = Validator::make($data, [
-            'user_id' => 'required|integer',
-            'motivo' => 'required|string',
-            'num_voo' => 'required|string',
-            'dta_voo' => 'required|date',
-            'detalhe' => 'required|string',
-            'status' => 'required|string',
-        ])->validate();
-
-        return $this->solicitationRepository->create($validated)->toArray();
+        return $this->solicitationRepository->create($data)->toArray();
     }
 }

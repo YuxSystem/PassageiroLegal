22b23da9fc9c015e6756f1b2fa26c8c184035765
Julio Santana <julio.santana@justa.com.vc>
2025-02-16 21:42:05 -0300

    feat(server): adiciona método de criação de solicitacao na abstracao da classe

--- app/Repositories/SolicitationRepository.php
+++ app/Repositories/SolicitationRepository.php
@@ -2,8 +2,11 @@
 
 namespace App\Repositories;
 
+use App\Models\Solicitation;
 use Illuminate\Database\Eloquent\Collection;
 
 interface SolicitationRepository {
     public function getAll(): Collection;
+
+    public function create(array $data): Solicitation;
 }

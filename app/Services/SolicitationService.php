d95679697eee18f1887520ce5f4e986583daf149
Julio Santana <julio.santana@justa.com.vc>
2025-02-22 23:55:04 -0300

    feat(server): cria método para buscar solicitacao por id na implementacao

--- app/Services/SolicitationService.php
+++ app/Services/SolicitationService.php
@@ -4,4 +4,8 @@ namespace App\Services;
 
 interface SolicitationService {
     public function getSolicitations(): array;
+
+    public function createSolicitation(array $data): array;
+
+    public function getSolicitation(string $id): array;
 }

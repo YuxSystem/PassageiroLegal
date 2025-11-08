89d6e2bd1437ec630f47904c13bf7283c921f03b
Mr. Guilherme Gomes <joe.chips@gmail.com>
2025-11-08 07:58:19 +0000

    Create SolicitationStatusHistory.php

---
+++ app/Models/SolicitationStatusHistory.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class SolicitationStatusHistory extends Model
+{
+    use HasFactory;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'solicitation_id',
+        'old_status',
+        'new_status',
+        'changed_by',
+        'notes',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function solicitation()
+    {
+        return $this->belongsTo(Solicitation::class, 'solicitation_id', 'id');
+    }
+
+    public function changedBy()
+    {
+        return $this->belongsTo(User::class, 'changed_by', 'id');
+    }
+
+    public static function booted()
+    {
+        static::creating(function ($model) {
+            $model->id = Str::uuid();
+        });
+    }
+}
+

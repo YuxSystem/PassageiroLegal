4993c8fba1df342ab881999a8eb37aac84052e9a
Mr. Guilherme Gomes <joe.chips@gmail.com>
2025-11-08 07:58:25 +0000

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Solicitation;
+use App\Observers\SolicitationObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Solicitation::observe(SolicitationObserver::class);
     }
 }

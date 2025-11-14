1b4e1208ec9b16a7505231d996248190d18e56b3
Mr. Guilherme Gomes <joe.chips@gmail.com>
2025-11-14 01:21:12 -0500

    Update app.tsx

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,6 +4,7 @@ import "../css/index.css"
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
 import Layout from "@/components/layout/Layout";
+import "./i18n/config";
 
 createInertiaApp({
   resolve: (name) => {
